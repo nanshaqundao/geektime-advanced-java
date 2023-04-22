91e825f3be7fa04edf0e324dee6beb2b5a7b16b9
nanshaqundao <nanshax@hotmail.co.uk>
2023-04-22 11:10:36 +0100

    best commit

---
+++ exercise/04/jvm-project-2/src/main/java/org/example/YoungOldArea.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class YoungOldArea {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        byte[] buffer = new byte[1024 * 1024 * 20]; //20MB
+    }
+}
